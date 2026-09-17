d7b5a669170c54ea662ba497811822768c8c1d43
Denkjt <dentisttvnol@gmail.com>
2026-09-17 22:26:30 +0700

    Change Google Drive download URL format
    Update acquisitionURL function to use new Google Drive URL format.

--- cmd/generate/main.go
+++ cmd/generate/main.go
@@ -165,7 +165,9 @@ func children(b Branch, parent string) []FileEntry {
 }
 
 func acquisitionURL(f FileEntry) string {
-	return "https://drive.google.com/uc?export=download&id=" + url.QueryEscape(f.ID)
+    return "https://drive.usercontent.google.com/download?id=" +
+        url.QueryEscape(f.ID) +
+        "&export=download&confirm=t"
 }
 
 func writeWeb(out, base string, branches []Branch) error {
