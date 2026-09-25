488add819de719af96328f194f0b875755ce67eb
Denkjt <dentisttvnol@gmail.com>
2026-09-25 10:07:51 +0700

    Add support for font file extensions in validation

--- internal/drive/drive.go
+++ internal/drive/drive.go
@@ -142,7 +142,7 @@ func isSupportedBook(f File) bool {
 		return false
 	}
 	switch strings.ToLower(filepath.Ext(f.Name)) {
-	case ".epub", ".mobi", ".azw3", ".cpfont":
+	case ".epub", ".mobi", ".azw3", ".cpfont", ".ttf", ".otf":
 		return true
 	default:
 		return false
